0dd96d1c7700e19fb7a02fe0abc902ee7aff9469
Rheeeee <rhee@akumau.biz.id>
2024-06-22 11:29:08 +0700

    Fix Telegram

--- index.js
+++ index.js
@@ -1,18 +1,100 @@
 const axios = require('axios');
 const TelegramBot = require('node-telegram-bot-api');
+const puppeteer = require("puppeteer");
 
-const telegramToken = '6340014452:AAFNnMkAquX-VsUnSDXMHZC4SLrfiNwVi1M';
-const chatId = '782106563'; // I
 
-const bot = new TelegramBot(telegramToken, {
+const config = {
+    chromePath: 'C:/Program Files/Google/Chrome/Application/chrome.exe',
+    telegramToken: '6340014452:AAFNnMkAquX-VsUnSDXMHZC4SLrfiNwVi1M',
+    chatId: '782106563',
+    token: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpYXQiOjE3MTkwMjM3MTAsInJvbGVzIjpbIlJPTEVfVVNFUiJdLCJhZGRyZXNzIjoic3BhbW5ldGZsaXhAbmF2YWxjYWRldHMuY29tIiwiaWQiOiI2NjcyMTZjNTBkYzcxMDZlOGMwYTI1ZDQiLCJtZXJjdXJlIjp7InN1YnNjcmliZSI6WyIvYWNjb3VudHMvNjY3MjE2YzUwZGM3MTA2ZThjMGEyNWQ0Il19fQ.UoXUaDVk3OU-Tyfa-VLbdDfKDfYH9b_7rpINs0YVtYqcASFl7oN9jzqC6vmwAXr37y6n5NvSVeNPFsHpg_ZCaQ'
+};
+
+const bot = new TelegramBot(config.telegramToken, {
     polling: true
 });
+
 
 // URL API mail.tm untuk mendapatkan pesan
 const mailApiUrl = 'https://api.mail.tm/messages';
 
 let lastMessageId = null;
 
+
+async function OpenNetflix(url, notificationText) {
+
+
+
+    // Launch browser
+    const browser = await puppeteer.launch({
+        headless: true,
+        executablePath: config.chromePath,
+        args: ["--incognito"],
+    });
+
+    // Open a new page
+    const page = await browser.newPage();
+
+    // Function to extract data from the verify page
+    const extractData = async (page) => {
+        try {
+            await page.waitForSelector(
+                "#appMountPoint > div > div > div > div.bd > div > div > div > div.challenge-code", {
+                    timeout: 5000
+                }
+            );
+
+            const data = await page.evaluate(() => {
+                const element = document.querySelector(
+                    "#appMountPoint > div > div > div > div.bd > div > div > div > div.challenge-code"
+                );
+                return element ? element.innerText : null;
+            });
+
+            console.log("Data:", data);
+            notificationText += `\nKode Netflix: ${data}`;
+            bot.sendMessage(config.chatId, notificationText);
+            // bot.sendMessage(config.chatId, data);
+        } catch (error) {
+            console.error("Error:", error.message);
+        }
+
+    };
+
+    // Function to click the button on the update location page
+    const clickButton = async (page) => {
+        try {
+            await page.waitForSelector(
+                "#appMountPoint > div > div > div > div.bd > div > div > div > div:nth-child(5) > button", {
+                    timeout: 5000
+                }
+            );
+
+            await page.click(
+                "#appMountPoint > div > div > div > div.bd > div > div > div > div:nth-child(5) > button"
+            );
+            console.log("Button clicked");
+        } catch (error) {
+            console.error("Error:", error.message);
+        }
+    };
+
+    // Navigate to the URL and perform the appropriate action
+    await page.goto(url, {
+        waitUntil: "domcontentloaded",
+    });
+
+    if (url.includes("/account/travel/verify")) {
+        await extractData(page);
+    } else if (url.includes("/account/update-primary-location")) {
+        await clickButton(page);
+    }
+
+    // Close the browser
+    await browser.close();
+
+
+}
 // Fungsi untuk mengambil detail pesan berdasarkan ID pesan
 async function getMessageDetails(messageId) {
     const messageDetailUrl = `https://api.mail.tm/messages/${messageId}`;
@@ -20,7 +102,7 @@ async function getMessageDetails(messageId) {
         const response = await axios.get(messageDetailUrl, {
             headers: {
                 // Tambahkan header yang diperlukan jika ada, misalnya authorization
-                'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpYXQiOjE3MTg3MDExNjcsInJvbGVzIjpbIlJPTEVfVVNFUiJdLCJhZGRyZXNzIjoibmV0cGxpeEBuYXZhbGNhZGV0cy5jb20iLCJpZCI6IjY2NzEzNThlODk3MDRmOWI1NTAzMGE1YiIsIm1lcmN1cmUiOnsic3Vic2NyaWJlIjpbIi9hY2NvdW50cy82NjcxMzU4ZTg5NzA0ZjliNTUwMzBhNWIiXX19.TYwgZdsIqPFFegAc9JsEfczJLFY1Izu2O0lLA4oz0hK1UNwo1lgfskatWVErdDRnkogqX3i0QI0EQyp7aFEm5'
+                'Authorization': `${config.token}`
             }
         });
         return response.data;
@@ -36,7 +118,7 @@ async function checkMessages() {
         const response = await axios.get(mailApiUrl, {
             headers: {
                 // Tambahkan header yang diperlukan jika ada, misalnya authorization
-                'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpYXQiOjE3MTg3MDExNjcsInJvbGVzIjpbIlJPTEVfVVNFUiJdLCJhZGRyZXNzIjoibmV0cGxpeEBuYXZhbGNhZGV0cy5jb20iLCJpZCI6IjY2NzEzNThlODk3MDRmOWI1NTAzMGE1YiIsIm1lcmN1cmUiOnsic3Vic2NyaWJlIjpbIi9hY2NvdW50cy82NjcxMzU4ZTg5NzA0ZjliNTUwMzBhNWIiXX19.TYwgZdsIqPFFegAc9JsEfczJLFY1Izu2O0lLA4oz0hK1UNwo1lgfskatWVErdDRnkogqX3i0QI0EQyp7aFEm5'
+                'Authorization': `${config.token}`
             }
         });
         const messages = response.data['hydra:member'];
@@ -60,18 +142,18 @@ async function checkMessages() {
                         const match = text.match(urlRegex);
 
                         let notificationText = `
-                        Pesan Baru!
-                        ID: ${latestMessage.id}
-                        Dari: ${latestMessage.from.address}
-                        Subjek: ${latestMessage.subject}
+                        Pesan Baru!\nID: ${latestMessage.id}\nDari: ${latestMessage.to[0].address}\nSubjek: ${latestMessage.subject}
                         `;
 
                         if (match) {
-                            notificationText += `\nURL Netflix: ${match[0]}`;
+                            // const uurl = match[0]
+
+                            await OpenNetflix(match[0], notificationText)
+                            // console.log(kode)
                         }
 
                         // Kirim notifikasi ke Telegram
-                        bot.sendMessage(chatId, notificationText);
+                        // bot.sendMessage(config.chatId, notificationText);
                     } else {
                         console.error('Pesan tidak memiliki teks.');
                     }
